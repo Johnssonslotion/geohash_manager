78c782a3548bc1ddc5e58d6497459546e48cd587
John Park <johnpark@johnssonslab.info>
2023-09-07 12:38:19 +0900

    [update] new_type

--- geohash_manager/__init__.py
+++ geohash_manager/__init__.py
@@ -1,3 +1,3 @@
 ## 패키지화 마무리 하기위해 import 시 초기값 설정
 from .main import GeohashManager
-from .model.shape_model import CircleShape, RectShape
+from .model.shape_model import CircleShape, RectShape, GeohashObject, Geohashes
